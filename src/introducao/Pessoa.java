684fdf303ff5f0e9bae5893ce65a74993a835927
Emmanuel Neri <emmanuelnerisouza@gmail.com>
2019-02-21 21:16:09 -0300

    Introdução - Aula 1

---
+++ src/introducao/Pessoa.java
@@ -0,0 +1,12 @@
+package introducao;
+
+public class Pessoa {
+
+    private String nome = "Nome";
+    private String sobrenome = "Sobrenome";
+
+    public String nomeCompleto() {
+        return nome + " " + sobrenome;
+    }
+
+}
---
+++ src/introducao/TestePessoa.java
@@ -0,0 +1,10 @@
+package introducao;
+
+public class TestePessoa {
+
+    public static void main(String args[]) {
+        Pessoa p = new Pessoa();
+        System.out.println(p.nomeCompleto());
+    }
+
+}
